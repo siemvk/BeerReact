2bc9ede9b1f25a721a1316e3d8e632aac96b2d9b
Siemvk <100954549+siemvk@users.noreply.github.com>
2026-08-29 19:39:46 +0200

    depracate flex
    Signed-off-by: Siemvk <100954549+siemvk@users.noreply.github.com>

--- src/Navs/Flex/Flex.tsx
+++ src/Navs/Flex/Flex.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { align } from "../../types";
 
 
+/**
+ * @deprecated FlexProps is deprecated and will be removed in a future release.
+ */
 export interface FlexProps extends React.HTMLAttributes<HTMLElement> {
     align?: align,
     directions?: "horizontal" | "vertical"
     // className?: string
 }
 
-
+/**
+ * @deprecated Flex is deprecated and will be removed in a future release.
+ */
 export const Flex = ({
     align = "left-align",
     directions = "horizontal",
